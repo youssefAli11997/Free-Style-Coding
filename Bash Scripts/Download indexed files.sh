14fd78c9dd8acff024d5fd1cf8b43304db884428
Youssef Ali <youssefali6212@gmail.com>
2017-12-30 11:58:10 +0200

    Create Download indexed files.sh

---
+++ Bash Scripts/Download indexed files.sh
@@ -0,0 +1,26 @@
+: '
+
+A simple script that can be used to download indexed files from a directory with wget
+I used it do download Quran audio files as a demonstration
+
+'
+directory="https://archive.org/download/Nasr-Qatami/"
+extension=".mp3"
+index=""
+
+for (( i=1; i <= 114; i++ ))
+do
+	if [ "$i" -lt "10" ]
+	then
+		index="00$i"
+	else
+		if [ "$i" -lt "100" ]
+		then
+			index="0$i"
+		else
+			index="$i"
+		fi
+	fi
+	wget -O "/media/youssefali/All/Quran/$index$extension" "$directory$index$extension"
+	#echo "$directory$index$extension"
+done
